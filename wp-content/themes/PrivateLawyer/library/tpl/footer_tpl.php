e474a815b3e568a0ed79426fd921c991ad79a82b
Jon <jon@onwebcentric.com>
2013-01-24 12:30:17 -0800

    Footer columns

--- wp-content/themes/PrivateLawyer/library/tpl/footer_tpl.php
+++ wp-content/themes/PrivateLawyer/library/tpl/footer_tpl.php
@@ -4,6 +4,17 @@
 <?php if (function_exists('dynamic_sidebar') && dynamic_sidebar('Footer Above')){?>
 <?php } else {?>  <?php }?>    
 		<div id="footer">
+            <div class="row">
+                <div class="column column-3">
+                    <h3>About</h3>
+                </div>
+                <div class="column column-3">
+                    <h3>Support</h3>
+                </div>
+                <div class="column column-3">
+                    <h3>Contact</h3>
+                </div>
+            </div>
         	<div class="footer_inner"> 
                 <p class="fl">Copyright &copy; 2011 - <a href="<?php bloginfo('home'); ?>"><?php bloginfo('name'); ?></a>. 126 W. 12300 South, Suite A,
 Draper, Utah 84020</p>
