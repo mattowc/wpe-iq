022b91de88724ae91b37366d1589d14f9acf971d
Jon <jon@onwebcentric.com>
2013-01-25 10:35:53 -0800

    Herro2

--- wp-content/plugins/jm-forms/init-form.php
+++ wp-content/plugins/jm-forms/init-form.php
@@ -6,6 +6,10 @@
  * @author Jonathon McDonald <jon@onewebcentric.com>
  */
 
+add_action('wp_head', 'check_forms');
+
+function check_forms() {
 if( isset( $_POST['jam-form-hidden'] ) ) {
 	wp_mail( 'jon@onewebcentric.com', $_POST['your_subject'], $_POST['your_email'] );
+}
 }
