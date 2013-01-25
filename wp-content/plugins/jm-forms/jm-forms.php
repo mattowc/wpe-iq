c9f194ac4f69fb31e1d433c0317222a57643c23d
Jon <jon@onwebcentric.com>
2013-01-25 09:48:58 -0800

    Basic jm-forms structure

---
+++ wp-content/plugins/jm-forms/jm-forms.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ Plugin Name:  Jam Forms
+ Plugin URI:  http://onewebcentric.com
+ Description:  We're jammin.  
+ Author:  Jonathon McDonald <jon@onewebcentric.com>
+ Author URI:  http://onewebcentric.com
+ Version: 0.01
+*/
+
+require_once( 'init-form.php' );
+require_once( 'classes/class-jam-form-shortcode.php' );
+?>
