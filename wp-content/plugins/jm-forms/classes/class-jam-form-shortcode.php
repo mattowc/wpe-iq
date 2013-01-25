00a6dc0bd0783053b1db5a9abf430ad72da875d2
Jon <jon@onwebcentric.com>
2013-01-25 10:02:04 -0800

    Includes basic shortcode parsing

--- wp-content/plugins/jm-forms/classes/class-jam-form-shortcode.php
+++ wp-content/plugins/jm-forms/classes/class-jam-form-shortcode.php
@@ -1,4 +1,30 @@
 <?php
 /**
- * Silence is golden or something?
+ * Adds shortcodes for specific forms.  
+ * 
+ * @author Jonathon McDonald <jon@onewebcentric.com>
  */
+
+class Jam_Form_Shortcode 
+{
+	function __construct() 
+	{
+		add_shortcode( 'include-form', array( &$this, 'output_html' ) );
+	}
+
+	function output_html($atts, $content="") 
+	{
+		extract(
+			shortcode_atts(
+				array(
+					'name' => 'first-form'
+				)
+			)
+		);
+
+		$file = file_get_contents( '../view/' . $name . '.html', FILE_USE_INCLUDE_PATH );
+		return $file;
+	}
+}
+
+new Jam_Form_Shortcode();
--- wp-content/plugins/jm-forms/init-form.php
+++ wp-content/plugins/jm-forms/init-form.php
@@ -1,4 +1,12 @@
 <?php
 /**
- * Silence is golden, or something?
+ * This will help us quickly test how easy or hard it is
+ * to write our own custom forms.  
+ *
+ * @author Jonathon McDonald <jon@onewebcentric.com>
  */
+
+function proccess_forms()
+{
+	
+}
