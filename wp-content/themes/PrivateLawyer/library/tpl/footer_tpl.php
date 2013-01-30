9022935435f422e2bc1f7b7790a6f9cd8d8f5035
Jon <jon@onwebcentric.com>
2013-01-30 12:31:50 -0800

    Image in p tag for footer

--- wp-content/themes/PrivateLawyer/library/tpl/footer_tpl.php
+++ wp-content/themes/PrivateLawyer/library/tpl/footer_tpl.php
@@ -7,8 +7,7 @@
             <div class="row">
                 <div class="column column-3">
                     <h3>About</h3>
-                    <img width="86" height="64" src="http://onewebcentric.wpengine.com/wp-content/uploads/2012/10/iq-logo.png" class="logo">
-                    <p>IQ Express, by Learning Technics, is a learning program for treating and curing learning problems like ADHD, Dyslexia, Auditory Processing Disorder, Dysgraphia, and other learning disabilities. It uses a technique called Physio-Neuro therapy to help rewire the brain. Fully utilizing the brain's ability to heal itself, neuroplasticity.</p>
+                    <p><img width="86" height="64" src="http://onewebcentric.wpengine.com/wp-content/uploads/2012/10/iq-logo.png" class="logo">IQ Express, by Learning Technics, is a learning program for treating and curing learning problems like ADHD, Dyslexia, Auditory Processing Disorder, Dysgraphia, and other learning disabilities. It uses a technique called Physio-Neuro therapy to help rewire the brain. Fully utilizing the brain's ability to heal itself, neuroplasticity.</p>
                 </div>
                 <div class="column column-3">
                     <h3>Support</h3>
@@ -18,7 +17,7 @@
                 <div class="column column-3">
                     <h3>Contact</h3>
                     <ul>
-                        <li>Phone: 800-893-9315</li>
+                        <li>Phone: 800-893-9315</li>s
                         <li>Email: support@iq-express.com</li>
                         <li>Hours: M-F 10-6pm MST</li>
                     </ul>
