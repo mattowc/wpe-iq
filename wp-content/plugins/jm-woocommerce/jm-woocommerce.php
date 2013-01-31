80e8e3f6029542fdc1e089f8dd731dff398d9ae5
Jon <jon@onwebcentric.com>
2013-01-31 11:00:57 -0800

    Starting to make fixes in plugin

---
+++ wp-content/plugins/jm-woocommerce/jm-woocommerce.php
@@ -0,0 +1,29 @@
+<?php
+/* 
+Plugin name:  JAM WooCommerce
+Plugin URI:  http://onewebcentric.com
+Description:  Extends WooCommerce Subscriptions to work properly with our monthly payment schema.  
+Author:  Jonathon McDonald
+Author URI:  http://onwebcentric.com
+*/
+
+$jm_order_shipping = 0;
+
+function jm_process_order( $order_id, $this->posted )
+{
+	global $jm_order_shipping;
+
+	if( WC_Subscriptions_Order::order_contains_subscription( $order ) && !WC_Subscriptions_Renewal_Order::is_renewal( $order->id ) )
+	{
+		add_filter( 'woocommerce_order_amount_total', 'jm_fix_price' );
+		$jm_order_shipping = $order->order_shipping;
+	}
+}
+
+add_action( 'woocommerce_checkout_update_order_meta', 'jm_process_order', 10, 2 );
+
+function jm_fix_price( $string ) {
+	global $jm_order_shipping;
+
+	return ( $string + $jm_order_shipping );
+}
