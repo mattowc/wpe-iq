d149fd25cae7b56af2a0b082bceff0783a37cb8b
Jon <jon@onwebcentric.com>
2013-01-31 09:50:11 -0800

    Test

--- wp-content/plugins/woocommerce-gateway-stripe/classes/class-wc-gateway-stripe-subscriptions.php
+++ wp-content/plugins/woocommerce-gateway-stripe/classes/class-wc-gateway-stripe-subscriptions.php
@@ -113,12 +113,7 @@ class WC_Gateway_Stripe_Subscriptions extends WC_Gateway_Stripe {
 	 */
 	function scheduled_subscription_payment( $amount_to_charge, $order, $product_id ) {
 
-		// Added by Jonathon McDonald
-		// Subtracts out shipping total, and charges less than that. 
-		if( WC_Subscriptions_Renewal_Order::is_renewal( $order->id ) ) 
-		{
-			$amount_to_charge = $amount_to_charge - $order->order_shipping;
-		}
+
 
 		$result = $this->process_subscription_payment( $order, $amount_to_charge );
 		
