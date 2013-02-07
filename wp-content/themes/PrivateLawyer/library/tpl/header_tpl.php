373784bf514e66809feb8246fff31e9359eaac7e
Jon McDonald <jon@onewebcentric.com>
2013-02-07 12:23:44 -0700

    Fixes most styling issues

--- wp-content/themes/PrivateLawyer/library/tpl/header_tpl.php
+++ wp-content/themes/PrivateLawyer/library/tpl/header_tpl.php
@@ -5,7 +5,7 @@
 <div id="info-header">
     <div id="info-container">
         <div id="info-wrap">
-            Call toll free (800) 893-9315    or    <a href="#" id="info-btn">Get Started</a>
+            Call toll free (800) 893-9315 <span>or</span> <a href="#" id="info-btn">Get Started</a>
         </div> <!-- End #info-wrap -->
     </div> <!-- End #info-container -->
 </div> <!-- End #info-header -->
